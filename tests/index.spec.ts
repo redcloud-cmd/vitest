43efdaa901cea65f029e500a5336cdcd2092d791
unknown <3478437011@qq.com>
2023-12-15 08:20:05 +0800

    全局配置vitest，但是无效

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,9 @@
-import {test,expect} from 'vitest'
+// import {test,expect} from 'vitest'
+import {sum} from './index.ts'
 test('first test',()=>{
         expect(true).toBe(true)
     }
 )
+test('sum test',()=>{
+    expect(sum(1,2)).toBe(3)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import {vitestConfig} from './vitestConfig'
 // https://vitejs.dev/config/
 export default defineConfig({
+  test:{
+    globals:true
+    // ...vitestConfig
+  },
   plugins: [vue()],
 })
